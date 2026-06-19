e2b9231016c92ace42e1c77b16738ddc5ca1f8a7
Pranshu Swami <pranshuswami185@gmail.com>
2026-06-19 15:00:20 +0530

    backend of amazon clone

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const categoryRoutes = require("./routes/categoryRoutes");
+const productRoutes = require("./routes/productRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/categories", categoryRoutes);
+app.use("/products", productRoutes);
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on ${PORT}`);
+});
